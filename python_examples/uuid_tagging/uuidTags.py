632e12bfdc2acc067121a7f32ba2dc6d1505a908
james sullivan <james.sullivan@cybergrx.com>
2023-01-03 14:13:16 -0700

    changed to API to prod endpoint

--- python_examples/uuid_tagging/uuidTags.py
+++ python_examples/uuid_tagging/uuidTags.py
@@ -68,7 +68,7 @@ def applyTags():
     companies = mapTags(wb)
     
     # define the api and token 
-    api = os.environ.get("CYBERGRX_API", "https://api-version-1.develop.new-staging.grx-dev.com").rstrip("/")
+    api = os.environ.get("CYBERGRX_API", "https://api.cybergrx.com").rstrip("/")
     token = os.environ.get("CYBERGRX_API_TOKEN", None)
 
     # using tqdm as a decerator to display status bar 
